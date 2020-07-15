b9c7e52211cae97eb1365910e7e714e2af858b0f
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2020-07-15 18:11:20 -0300

    Primer conexion a la BD desde Java Exitosa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
-
-
-
+import com.complexible.stardog.api.Connection;
+import com.complexible.stardog.api.ConnectionConfiguration;
+import com.complexible.stardog.api.admin.AdminConnection;
+import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
 
 public class Main {
 
     public static void main(String[] args) {
+        createConnection();
+
+    }
+
+    //Crea una conexion a la base de datos TPOntologias en el servidor localhost:5820
+    public static void createConnection() {
+        try (final Connection aConn = ConnectionConfiguration.to("TPOntologias").server("http://localhost:5820")
+                .credentials("admin", "admin")
+                .connect()) {
+
+            System.out.println(aConn.name());
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
