cd23507a76c3db0d18ec610bea0fa8274c48a26d
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2020-07-15 20:22:37 -0300

    Primer query de dos condiciones a la BD desde Java Exitosa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import com.complexible.common.rdf.query.resultio.TextTableQueryResultWriter;
 import com.complexible.stardog.api.*;
 import com.stardog.stark.query.SelectQueryResult;
+import com.stardog.stark.query.io.QueryResultWriter;
 import com.stardog.stark.query.io.QueryResultWriters;
 
 public class Main {
@@ -12,15 +13,21 @@ public class Main {
     //Crea una conexion a la base de datos TPOntologias en el servidor localhost:5820
     public static void createConnection() {
         try (final Connection aConn = ConnectionConfiguration.to("TPOntologias").server("http://localhost:5820")
+                .reasoning(true)
                 .credentials("admin", "admin")
                 .connect()) {
 
             System.out.println(aConn.name());
 
-            //Hacer una Query
+            //Hacer una Query simple
             SelectQuery query = aConn.select("select * {?a a TPADMR:Alumno}");
             SelectQueryResult selectQueryResult = query.execute();
             QueryResultWriters.write(selectQueryResult, System.out, TextTableQueryResultWriter.FORMAT);
+
+            //Hacer una Query con 2 condiciones
+            SelectQuery query_1 = aConn.select("select * {?a a TPADMR:Alumno. ?a TPADMR:tienePonderacion ?b}");
+            SelectQueryResult selectQueryResult_1 = query_1.execute();
+            QueryResultWriters.write(selectQueryResult_1, System.out, TextTableQueryResultWriter.FORMAT);
 
         } catch (Exception e){
             e.printStackTrace();
