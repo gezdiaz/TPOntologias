9ff5e3ea3cb5e7fd41d72dfb43b6534588223d6a
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2020-07-15 18:36:53 -0300

    Primer query a la BD desde Java Exitosa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
-import com.complexible.stardog.api.Connection;
-import com.complexible.stardog.api.ConnectionConfiguration;
-import com.complexible.stardog.api.admin.AdminConnection;
-import com.complexible.stardog.api.admin.AdminConnectionConfiguration;
+import com.complexible.common.rdf.query.resultio.TextTableQueryResultWriter;
+import com.complexible.stardog.api.*;
+import com.stardog.stark.query.SelectQueryResult;
+import com.stardog.stark.query.io.QueryResultWriters;
 
 public class Main {
 
     public static void main(String[] args) {
         createConnection();
-
     }
 
     //Crea una conexion a la base de datos TPOntologias en el servidor localhost:5820
@@ -17,6 +16,11 @@ public class Main {
                 .connect()) {
 
             System.out.println(aConn.name());
+
+            //Hacer una Query
+            SelectQuery query = aConn.select("select * {?a a TPADMR:Alumno}");
+            SelectQueryResult selectQueryResult = query.execute();
+            QueryResultWriters.write(selectQueryResult, System.out, TextTableQueryResultWriter.FORMAT);
 
         } catch (Exception e){
             e.printStackTrace();
