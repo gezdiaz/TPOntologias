a6e45d9ad55c39db029f14cad4813e20f138531d
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2020-07-23 19:32:28 -0300

    Primer Interfaz

---
+++ src/main/java/PruebaPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class PruebaPanel {
+    private JTextField ingresoPromMin;
+    private JTextField ingresoCantMat;
+    private JLabel labelPromMin;
+    private JLabel labelCantMat;
+    private JLabel titulo;
+    private JButton botonAceptar;
+}
