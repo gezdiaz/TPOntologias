6f738037c91012125533de3736c8d579a0675a63
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2020-08-08 18:16:48 -0300

    Prueba numero 2 de extraccion de datos
    Signed-off-by: HerniiGoH <calbornoz@frsf.utn.edu.ar>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.complexible.common.rdf.query.resultio.TextTablePathQueryResultWriter;
 import com.complexible.common.rdf.query.resultio.TextTableQueryResultWriter;
 import com.complexible.stardog.api.*;
 import com.complexible.stardog.jena.SDJenaFactory;
+import com.stardog.stark.query.BindingSet;
 import com.stardog.stark.query.SelectQueryResult;
 import com.stardog.stark.query.io.QueryResultWriter;
 import com.stardog.stark.query.io.QueryResultWriters;
@@ -14,6 +15,8 @@ import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.binding.Binding;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -40,23 +43,28 @@ public class Main {
             SelectQueryResult selectQueryResult_1 = query_1.execute();
             QueryResultWriters.write(selectQueryResult_1, System.out, TextTableQueryResultWriter.FORMAT);*/
 
-            //Dataset dataset = SDJenaFactory.createDataset(aConn);
+
+            /*Dataset dataset = SDJenaFactory.createDataset(aConn);
             Model model = SDJenaFactory.createModel(aConn);
 
             model.begin();
-            model.commit();
+            model.commit();*/
 
             StringBuffer qStrBf = new StringBuffer()
                     .append("PREFIX TPADMR: <http://www.semanticweb.org/gasto/ontologies/2020/4/TPADMR#>")
-                    .append("select ?nom ?leg WHERE")
+                    .append("select ?nom ?leg ?pond ?edad ?carr ?cmat WHERE")
                     .append("{")
                     .append(" ?a a TPADMR:Alumno.")
                     .append(" ?a TPADMR:tieneNombre ?nom.")
                     .append(" ?a TPADMR:tieneLegajo ?leg.")
+                    .append(" ?a TPADMR:tieneEdad ?edad.")
+                    .append(" ?a TPADMR:tieneCarrera ?carr.")
+                    .append(" ?a TPADMR:tieneCantidadMateriasAprobadas ?cmat.")
+                    .append(" ?a TPADMR:tienePonderacion ?pond.")
                     .append("}");
 
-            Query query = QueryFactory.create(qStrBf.toString());
-            QueryExecution execution = QueryExecutionFactory.create(query, model);
+            /*Query query = QueryFactory.create(qStrBf.toString());
+            QueryExecution execution = QueryExecutionFactory.create(query, dataset);
             ResultSet resultSet = execution.execSelect();
             while(resultSet.hasNext()){
                 Binding binding = resultSet.nextBinding();
@@ -65,6 +73,17 @@ public class Main {
                     System.out.println(binding.get(iterator.next()));
                 }
                 System.out.println("Sale del while interno");
+            }*/
+
+            SelectQuery selectQuery = aConn.select(qStrBf.toString());
+            SelectQueryResult selectQueryResult = selectQuery.execute();
+            while(selectQueryResult.hasNext()){
+                BindingSet bindingSet = selectQueryResult.next();
+                Set<String> strings = bindingSet.variables();
+                for (String s : strings){
+                    System.out.println(s+": "+bindingSet.get(s));
+                }
+                System.out.println("Salgo del For");
             }
 
 
